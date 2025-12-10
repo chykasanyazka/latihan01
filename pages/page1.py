0cfee31edb64d8a87dcca5eb477df747e28c9c94
chykasanyazka <chykasanyazka25@gmail.com>
2025-12-10 18:35:32 +0700

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
-st.title("Welcome to the Chyka's Home Page")
-st.write("Ini demo aplikasi streamlit.")
+# Konfigurasi halaman
+st.set_page_config(
+    page_title="Proyek Kelompok Koding Kiding",
+    layout="wide"
+)
+
+# Judul halaman
+st.title("Proyek Kelompok Koding Kiding")
+
+# Deskripsi atau teks tambahan
+st.write("Data penjualan Kopi Kenangan merupakan kumpulan informasi transaksi 
+yang tercatat pada setiap gerai Kopi Kenangan dalam periode tertentu. Data ini digunakan untuk menganalisis 
+performa penjualan, perilaku pelanggan, tren produk, serta efektivitas strategi pemasaran di seluruh cabang.")
