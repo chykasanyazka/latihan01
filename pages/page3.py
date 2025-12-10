6ff69b24d7ffb6069f5fa64fc12f5a09e5d1a233
Chyka <chykasanyazka25@gmail.com>
2025-12-10 16:58:17 +0700

    Menyelesaikan tugas load DF di page3.

--- pages/page3.py
+++ pages/page3.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Settings")
 st.write("This page allows you to customize your preferences.")
+
+df = pd.read_csv('data/datakopikenangan.csv')
+st.write(df.tail())
+
+st.write("Simulasi error")
